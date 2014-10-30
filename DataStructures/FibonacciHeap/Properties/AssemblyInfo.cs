7aefcb571cf6ff833739f59e824a358a4068edf4
sternze <sternze@gmail.com>
2014-10-30 18:38:13 +0100

    initial commit; finished Fibonacci Trees; Still need to write TestCases and test the whole implementation

---
+++ DataStructures/FibonacciHeap/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataStructures")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sternze")]
+[assembly: AssemblyProduct("DataStructures")]
+[assembly: AssemblyCopyright("Copyright © Daniel Sternig - Sternze 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6874cf91-967f-4bc5-831b-e81e2192f6c9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,16 @@
+<h1>Data Structures</h1>
+The Folder DataStructures conains a C# .NET Class Library, which implements uncommon datastructures.
+
+<h2>Currently Implemented</h2>
+<ul>
+	<li>Fibonacci Trees</li>
+</ul>
+
+<h2>Upcoming Implementations</h2>
+<ul>
+	<li>Binary Search Trees</li>
+	<li>AVL Trees</li>
+	<li>Red-Black Trees</li>
+	<li>B & B+ Trees</li>
+	<li>van Emde Boas (vEB) Trees</li>
+</ul>
